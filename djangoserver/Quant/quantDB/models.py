8b0f6d34fdcf72ea1c3169a01296d5b42db59c32
Henu <hyun0404woo@gmail.com>
2021-05-02 23:44:05 +0900

    DBmodel_add_class_Meta

--- djangoserver/Quant/quantDB/models.py
+++ djangoserver/Quant/quantDB/models.py
@@ -4,11 +4,22 @@ class Quarter(models.Model):
     stock_date = models.DateField(auto_now=False, auto_now_add=False)
     benefit = models.IntegerField()
     
+    class Meta:
+        verbose_name = "분기별 데이터"
+        ordering = ["stock_date"]
+    
 
 class Financial_Statements(models.Model):
     quarter = models.ForeignKey(Quarter, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "재무재표"
 
     
 class Company(models.Model):
     company_name = models.CharField(max_length=200, null=False)
     fs = models.ForeignKey(Financial_Statements, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "기업 이름"
+        ordering = ["company_name"]
