60b5b1d015933555ab00ab730ffe238afa20fe05
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-05-05 17:57:26 +0900

    error == True (WARNING)

--- djangoserver/Quant/quantDB/models.py
+++ djangoserver/Quant/quantDB/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # items = ["rcept_no","reprt_code","bsns_year","sj_div","sj_nm","account_nm","account_detail","thstrm_amount"]
 # item_names = ["접수번호","보고서코드","사업연도","재무제표구분","재무제표명","계정명","계정상세","당기금액"]
 
+# 객체 n개
 class Company(models.Model):
     company_name = models.CharField(max_length=200, null=False)
     
@@ -11,32 +12,42 @@ class Company(models.Model):
         verbose_name = "기업"
         ordering = ["company_name"]
 
-class Financial_Statements(models.Model):
-    sj_name = models.CharField(help_text="제무제표명", max_length=255, blank=False, null=False)
-    company = models.ForeignKey(Company, null=True, blank=True, on_delete=models.CASCADE)
-    
-    class Meta:
-        verbose_name = "재무재표명"
-
+# 객체 7개 (2015 ~ 2017)
 class Year(models.Model):
-    bsns_year = models.IntegerField(help_text="사업연도", blank=False, null=False)
-    financial_statements = models.ForeignKey(Financial_Statements, null=True, blank=True, on_delete=models.CASCADE)
+    bsns_year = models.IntegerField(help_text="사업연도", blank=True, null=True)
+    company = models.ForeignKey(Company, null=True, blank=True, on_delete=models.CASCADE)
     
     class Meta:
         verbose_name = "연도별 데이터"
 
+# 객체 4개 (1/4, 2/4, 3/4, 4/4)
 class Quarter(models.Model):
     # report_code = models.IntegerField(help_text="보고서코드(분기 구분)", blank=False, null=False)  # 분기를 판단
-    
     quarter_name = models.CharField(help_text="분기", max_length=30, blank=True, null=True)
-    sj_div = models.CharField(help_text="제무제표구분", max_length=255, blank=False, null=False)
-    sj_name = models.CharField(help_text="제무제표명", max_length=255, blank=False, null=False)
-    account_name = models.CharField(help_text="계정명", max_length=255, blank=False, null=False)
-    account_detail = models.CharField(help_text="계정상세", max_length=255, blank=False, null=False)
-    thstrm_amount = models.IntegerField(help_text="당기금액", blank=False, null=False)
-    
     year = models.ForeignKey(Year, null=True, blank=True, on_delete=models.CASCADE)
     
     class Meta:
         verbose_name = "분기별 데이터"
-        ordering = ["account_name"]
+        # ordering = ["account_name"]
+
+# 객체 2개 (linked or basic)
+class Financial_Statements_LinkOrBasic(models.Model):
+    linkOrbasic = models.CharField(max_length=30, blank=True, null=True)
+    quarter = models.ForeignKey(Quarter, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "연결/기본"
+
+# 객체 5개 (BS:재무상태표, IS:손익계산서, CIS:포괄손익계산서, CF:현금흐름표, SCE:자본변동표)
+class Financial_Statements_Div(models.Model):
+    # sj_name = models.CharField(help_text="재무제표명(재무상태표 손익계산서...)", max_length=255, blank=False, null=False)
+    sj_div = models.CharField(help_text="재무제표구분(BS IS ...)", max_length=255, blank=False, null=False)
+    lb = models.ForeignKey(Financial_Statements_LinkOrBasic, on_delete=models.CASCADE)
+    
+    class Meta:
+        verbose_name = "재무제표구분"
+
+class FS_Account(models.Model):
+    financial_statements_div = models.ForeignKey(Financial_Statements_Div, on_delete=models.CASCADE)
+    account_name = models.CharField(help_text="계정명", max_length=255, blank=False, null=False)
+    a = models.IntegerField(help_text="계정명에 대한 자산", blank=True, null=True)
--- djangoserver/Quant/quantDB/views.py
+++ djangoserver/Quant/quantDB/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Company, Financial_Statements, Quarter
+from .models import Company, Financial_Statements_LinkOrBasic,\
+     Financial_Statements_Div, Quarter, Year, FS_Account
 import json
 # Create your views here.
 
@@ -10,40 +11,72 @@ def find_state(request):
     # start_date = request.stdate
     # end_date = request.eddate
     
-    com_name_list = []
-    b_list = []
+    # com_name_list = []
+    # year_list = []
+    # quarter_list = []
     
-    for com in company_list:
-        fs_date = com.fs.quarter.stock_date
-        # if fs_date >= start_date and fs_date <= end_date:
-        # q_set = com.fs.quarter_set.all()
-        # for q in q_set:
-        b_list.append(com.fs.quarter.benefit)
-        com_name_list.append(com.company_name)
     
-    data_list = [b_list, com_name_list]
+    # for com in company_list:
+    #     b_list.append(com.fs.quarter.benefit)
+    #     com_name_list.append(com.company_name)
     
-    context = {'b_list' : b_list, 'com_name_list' : com_name_list}
+    # data_list = [b_list, com_name_list]
+    
+    context = {'company_list' : company_list}
     
     return render(request, 'main.html', context)
     
-# def make_financial_state(request):
-#     items = ["rcept_no","reprt_code","bsns_year","sj_div","sj_nm","account_nm","account_detail","thstrm_nm","thstrm_amount"]
-#     item_names = ["접수번호","보고서코드","사업연도","재무제표구분","재무제표명","계정명","계정상세","당기명","당기금액"]
+def make_financial_state(request):
+    items = ["rcept_no","reprt_code","bsns_year","sj_div","sj_nm","account_nm","account_detail","thstrm_nm","thstrm_amount"]
+    item_names = ["접수번호","보고서코드","사업연도","재무제표구분","재무제표명","계정명","계정상세","당기명","당기금액"]
     
-#     # 기업 이름도 같이 보내오게
-#     json_dic = crawlfunc()
-#     company = Company()
-#     fs = Financial_Statements()
-#     quarter = Quarter()
+    # 기업 이름도 같이 보내오게
+    json_dic, is_linked, cname = crawlfunc()
     
-#     if json_dic['status'] == "000":
-#         company.company_name = c_name
-#         company.save()
+    company = Company()
+    is_link = Financial_Statements_LinkOrBasic()
+    year = Year()
+    quarter = Quarter()
+    fs_div = Financial_Statements_Div()
+    fs_a = FS_Account()
+    
+    if json_dic['status'] == "000":
+        company.company_name = c_name
+        company.save()
         
-#         for fs_list in json_dict['list']:
-
-
+        for fs_list in json_dict['list']:
+            year.bsns_year = fs_list["bsns_year"]   # int로 변환해주기
+            if fs_list["reprt_code"] == "11013":
+                quarter.quarter_name = "1/4"
+            # elif
+            
+            
+            if(is_linked == "CFS"):
+                is_link.linkOrbasic = "linked"
+            else:
+                is_link.linkOrbasic = "basic"
+            
+            if fs_list["sj_div"] == "BS":
+                fs_div.sj_div = "BS"
+            elif fs_list["sj_div"] == "IS":
+                fs_div.sj_div = "IS"
+            
+            fs_a.account_name = fs_list["account_nm"]
+            fs_a.a = fs_list["thstrm_amount"]
+            
+            
+            fs_a.financial_statements_div = fs_div
+            fs_a.save()
+            fs_div.lb = is_link
+            fs_div.save()
+            is_link.quarter = quarter
+            is_link.save()
+            quarter.year = year
+            quarter.save()
+            year.company = company
+            year.save()
+            
+            
 def show_chart(request):
     chart_dataset = { 
         '1': 10, 
