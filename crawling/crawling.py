f43f0bb0b72bec5e22fcab2e569b879493fceed4
sihyeong671 <bshlab671@naver.com>
2021-05-04 23:51:29 -0700

    temp

--- crawling/crawling.py
+++ crawling/crawling.py
@@ -4,7 +4,7 @@ from io import BytesIO
 import zipfile
 import xml.etree.ElementTree as et
 import json
-from . import API_KEY
+import API_KEY
 
 # pd.set_option('display.max_row', 300) # 행 갯수 늘려서 보는 옵션
 # pd.set_option('display.max_column', 100) # 열 갯수 늘려서 보는 옵션 
@@ -19,9 +19,9 @@ def krx_corporation():
         "name": "fileDown",
         "url": "dbms/MDC/STAT/standard/MDCSTAT01901"
         }
-
+    # 본인의 user-agent 삽입
     headers = {
-        'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Whale/2.9.114.33 Safari/537.36'
+        'User-Agent' : API_KEY.USERAGENT
         }
 
     resp = rq.post(generate_url,generate_param)
@@ -33,6 +33,7 @@ def krx_corporation():
     _resp = rq.post(download_url, download_param, headers=headers)
     df = pd.read_excel(BytesIO(_resp.content))
     print(df)
+
 
 def dart_unique_key(api_key):
     # dart에서 고유번호 가져오기
@@ -47,10 +48,10 @@ def dart_unique_key(api_key):
     data_code = []
     for child in root:
         # 상장회사만 가져오기
-        # if len(child.find('stock_code').text.strip()) > 1:  # stock_code 없는 기업 제외
-        data_code.append([])
-        for item in items:
-            data_code[-1].append(child.find(item).text)
+        if len(child.find('stock_code').text.strip()) > 1:  # stock_code 없는 기업 제외
+          data_code.append([])
+          for item in items:
+              data_code[-1].append(child.find(item).text)
     df_dart = pd.DataFrame(data_code, columns=item_names)
     print(df_dart)
 
@@ -64,34 +65,23 @@ def financial_data(api_key, corp_code):
     # 연결재무제표없는 기업은 일반 재무제표를 가져와야 한다.
     res = rq.get(url, params)
     json_dict = json.loads(res.text)
-# 키	명칭	List 여부	출력설명
-# rcept_no	접수번호	Y	접수번호(14자리)
-# reprt_code	보고서 코드	Y	1분기보고서 : 11013
-# bsns_year	사업 연도	Y	2018
-# sj_div	재무제표구분	Y	BS : 재무상태표
-# sj_nm	재무제표명	Y	ex) 재무상태표 또는 손익계산서 출력
-# account_nm	계정명	Y	계정명칭 ex) 자본 총계
-# account_detail	계정상세	Y	※ 자본변동표에만 출력
-# - 자본 [member]|지배기업 소유주지분
-# - 자본 [member]|지배기업 소유주지분|기타포괄손익누계액 [member]
-# thstrm_nm	당기명	Y	ex) 제 13 기
-# thstrm_amount	당기금액	Y	9,999,999,999
-# ※ 분/반기 보고서이면서 (포괄)손익계산서 일 경우 [3개월] 금액
-    items = ["rcept_no","reprt_code","bsns_year","sj_div","sj_nm","account_nm","account_detail","thstrm_nm","thstrm_amount"]
-    item_names = ["접수번호","보고서코드","사업연도","재무제표구분","재무제표명","계정명","계정상세","당기명","당기금액"]
+    # print(json_dict)
+    items = ["rcept_no","reprt_code","bsns_year","sj_div","sj_nm","account_nm","account_detail","thstrm_amount"]
+    item_names = ["접수번호","보고서코드","사업연도","재무제표구분","재무제표명","계정명","계정상세","당기금액"]
     data_fi= []
     if json_dict['status'] == "000": # 정상적으로 데이터 가져옴
         for line in json_dict['list']: # 회사 한개씩 가져오기
             data_fi.append([])
             for itm in items:
-                if itm in line.keys(): # itm 키값 append
+                if itm in line.keys(): # itm append
                     data_fi[-1].append(line[itm])
                 else:
                     data_fi[-1].append('') # 내용없을시 공백
-    df_fianacial = pd.DataFrame(data_fi, columns =item_names)
+    df_fianacial = pd.DataFrame(data_fi, columns =item_names) 
     print(df_fianacial)
 
-api_key = API_KEY
+
+api_key = API_KEY.APIKEY
+financial_data(api_key,"00126380")
 # 모든 고유번호 가져오기
-dart_unique_key(api_key)
 # 모든 고유번호 for 문으로 돌리면서 재무제표 가져오기
--- djangoserver/Quant/quantDB/models.py
+++ djangoserver/Quant/quantDB/models.py
@@ -4,11 +4,9 @@ class Quarter(models.Model):
     stock_date = models.DateField(auto_now=False, auto_now_add=False)
     benefit = models.IntegerField()
     
-
 class Financial_Statements(models.Model):
     quarter = models.ForeignKey(Quarter, on_delete=models.CASCADE)
 
-    
 class Company(models.Model):
     company_name = models.CharField(max_length=200, null=False)
     fs = models.ForeignKey(Financial_Statements, on_delete=models.CASCADE)
