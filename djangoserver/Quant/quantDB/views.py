8c7130fc961f417430b25ab49a8fde2fff130740
Henu <hyun0404woo@gmail.com>
2021-05-22 22:59:38 +0900

    create search func

--- djangoserver/Quant/quantDB/views.py
+++ djangoserver/Quant/quantDB/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Company, FS_LoB,\
     FS_Div, Quarter, Year, FS_Account
 import json
+from django.http import HttpResponse
+from django.db.models import Q
 # Create your views here.
 
 
@@ -39,6 +41,22 @@ def show_chart(request):
         'chartJson' : chartJson,
     }
     return render(request, 'chart.html', context)
+    
+def search_companyname(request):
+    cname = request.GET.get('company_name', '')  # 회사명
+
+    company_list = Company.objects.order_by('-company_name')
+    if cname:
+        company = company_list.filter(
+            Q(company_name__icontains=cname) # 회사명 검색
+        ).distinct()
+    
+    
+    
+    context = {"company" : company, "cname" : cname}
+    return render(request, 'search.html', context)
+               
+    
 
 
 
