fc7fa7bf203f62fe48171eaf90cf2ca89b47794b
Henu <hyun0404woo@gmail.com>
2021-08-07 16:36:04 +0900

    test

--- DBmanageapp/views.py
+++ DBmanageapp/views.py
@@ -17,10 +17,10 @@ class View_All_Data(ListView):
   template_name='DBmanageapp/manager.html'
   paginat_by = 30
   
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      context["company_temp"] = Company.objects.get(company_name="엑세스바이오")
-      return context
+  # def get_context_data(self, **kwargs):
+  #     context = super().get_context_data(**kwargs)
+  #     context["company_temp"] = Company.objects.get(company_name="엑세스바이오")
+  #     return context
   
 def Crawling_Data(request):
   # Save_Dart_Data(api_key)
