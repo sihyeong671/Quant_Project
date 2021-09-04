6ebeb490df370027c455605022f59a99527c6b13
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-09-04 21:02:00 +0900

    Push

--- client/src/containers/profile/profile.js
+++ client/src/containers/profile/profile.js
@@ -1,16 +1,18 @@
 import Profile from '../../components/auth/profile/profile'
+import Rank from '../../components/pages/rank/rank';
 import React from 'react';
 import {connect}  from 'react-redux';
 
 
 function mapStateToProps(state){
-  return state
+  return state.user
 }
 
-function mapDispatchToProps(){
-
+function mapDispatchToProps(dispatch){
+  return {
+  }
 }
 
 
-export default connect(mapStateToProps, null)(Profile);
+export default connect(mapStateToProps, mapDispatchToProps)(Profile);
 
