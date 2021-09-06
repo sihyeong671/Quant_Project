677d4c540b26b50b73c0926a1deeb19e76228200
Henu <hyun0404woo@gmail.com>
2021-09-06 18:23:40 +0900

    category 생성은 superuser만

--- server/stockmanage/urls.py
+++ server/stockmanage/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import View_All_Data, Crawling_Data
 
-from stockmanage.apis import CompanyNameApi
+from stockmanage.apis import CompanyNameApi, DailyPriceApi
 
 app_name = "stockmanage"
 
@@ -12,4 +12,6 @@ urlpatterns = [
     path('crawling/', Crawling_Data, name="crawling"), 
     
     path('company', CompanyNameApi.as_view(), name="company_info"),
+    path('daily/<str:code>', DailyPriceApi.as_view(), name="daily_price"),
+    
 ]
