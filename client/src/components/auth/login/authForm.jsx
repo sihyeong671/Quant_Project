14aac9b51bfed022bd083c66eb8bc5043e8ab8ca
POBSIZ <65818008+POBSIZ@users.noreply.github.com>
2021-10-03 15:58:18 +0900

    fix

--- client/src/components/auth/login/authForm.jsx
+++ client/src/components/auth/login/authForm.jsx
@@ -20,10 +20,10 @@ import srcSet from "./assets/src";
 const authForm = (props) => (
   <div className="auth">
     <Switch>
-      <Route exact path ='/auth/login' render={() => <Login basicLogin={props.basicLogin}/>}></Route>
-      <Route path ='/auth/search_id' render={() => <SearchId searchId={props.searchId}/>}></Route>
-      <Route path ='/auth/search_pwd' render={() => <SearchPwd searchPwd={props.searchPwd} sendCode={props.sendCode}/>}></Route>
-      <Route path ='/auth/signup'  render={() => <SignUp basicSignUp={props.basicSignUp}/>}></Route>
+      <Route exact path='/auth/login' render={() => <Login basicLogin={props.basicLogin} />}></Route>
+      <Route path='/auth/search_id' render={() => <SearchId searchId={props.searchId} />}></Route>
+      <Route path='/auth/search_pwd' render={() => <SearchPwd searchPwd={props.searchPwd} sendCode={props.sendCode} />}></Route>
+      <Route path='/auth/signup' render={() => <SignUp basicSignUp={props.basicSignUp} />}></Route>
     </Switch>
 
     <div className='social-btn'>
@@ -36,7 +36,7 @@ const authForm = (props) => (
         <img src={srcSet.google} alt="googlelogin" />
         <span>구글로 로그인</span>
       </a>
-      
+
       <a className="social-naver" href='http://localhost:8000/api/v1/auth/login/naver'>
         <img src={srcSet.naver} alt="naverlogin" />
         <span>네이버로 로그인</span>
@@ -45,8 +45,8 @@ const authForm = (props) => (
 
     <div>
       <ul>
-        <li><Link to ='/auth/search_id'>아이디 찾기</Link></li>
-        <li><Link to ='/auth/search_pwd'>비밀번호 찾기</Link></li>
+        <li><Link to='/auth/search_id'>아이디 찾기</Link></li>
+        <li><Link to='/auth/search_pwd'>비밀번호 찾기</Link></li>
         <li><Link to='/auth/signup'>회원가입</Link></li>
       </ul>
     </div>
--- client/src/components/search/search.jsx
+++ client/src/components/search/search.jsx
@@ -1,11 +1,11 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { hot } from 'react-hot-loader';
 import './assets/css/style.scss';
 
 // 검색할 기업 보여주는 컴포넌트
 const List = (props) => {
     console.log('list rendering')
-    return(
+    return (
         <li>
             {props.name}
             <button onClick={() => props.onClick(props.id)}>❌</button>
@@ -13,7 +13,7 @@ const List = (props) => {
     )
 };
 
-const Search = (props) =>{
+const Search = (props) => {
     console.log('Search rendering');
     const [corpName, setCorpName] = useState('');
 
@@ -22,24 +22,24 @@ const Search = (props) =>{
         {
             name: '네이버',
             id: 2313
-        }, 
+        },
         {
             name: '삼성',
             id: 32456
-        }, 
+        },
         {
             name: 'Apple',
             id: 54823
-        }, 
+        },
         {
             name: 'Tesla',
             id: 97832
-        }, 
+        },
     ])
-    
+
     // 연관 검색어 리스트
     const [relList, setRelList] = useState([])
-    
+
     const [listStyle, setStyle] = useState({});
 
     useEffect(async () => {
@@ -47,34 +47,34 @@ const Search = (props) =>{
     }, [corpName]);
 
     // 연관 검색어 필터링
-    const filterRelList = async ()=>{
-        const filterList = await getList.filter(item=>{ 
-            return( 
-                item.name.toLowerCase().includes(corpName.toLowerCase()) 
+    const filterRelList = async () => {
+        const filterList = await getList.filter(item => {
+            return (
+                item.name.toLowerCase().includes(corpName.toLowerCase())
             )
         });
         console.log(filterList);
-        (corpName.length > 0 && filterList.length > 0) ? ( 
+        (corpName.length > 0 && filterList.length > 0) ? (
             await setRelList(filterList),
-            setStyle({display: 'block'})
-        ):(
+            setStyle({ display: 'block' })
+        ) : (
             await setRelList(),
-            setStyle({display: 'none'})
+            setStyle({ display: 'none' })
         )
     }
 
     // 연관 검색어 렌더
-    const renderRelList = () =>{
-        return(
+    const renderRelList = () => {
+        return (
             <ul className="search_relative-list" style={listStyle}>
-                {relList?.map((item, i)=>{
-                    return(
+                {relList?.map((item, i) => {
+                    return (
                         <li key={i}>
                             {item.name}
-                            <button onClick={ e => {
+                            <button onClick={e => {
                                 props.props.onClickCreate(
-                                    (props.props.search.id) + 1, 
-                                    item.name, 
+                                    (props.props.search.id) + 1,
+                                    item.name,
                                     props.props.search.corpList.length
                                 );
                                 setCorpName('');
@@ -92,31 +92,31 @@ const Search = (props) =>{
     }
 
     let message;
-    if ( props.props.search.corpList.length >= 4){
+    if (props.props.search.corpList.length >= 4) {
         message = <div>더 이상 추가 할 수 없습니다</div>
     }
-    else{
+    else {
         message = null
     }
 
-    return(
+    return (
         <section className="search-form">
-            <input className="search-input" type="text" placeholder="기업명" onChange={onChange}/>
+            <input className="search-input" type="text" placeholder="기업명" onChange={onChange} />
             {renderRelList()}
             {message}
 
             <div className="search_list">
                 {props.props.search.corpList.map((data) => (
-                    <List 
+                    <List
                         onClick={props.props.onClickDelete}
                         name={data.name}
-                        id = {data.id}
+                        id={data.id}
                         key={data.id}>
                     </List>
                 ))}
             </div>
         </section>
     )
-}   
+}
 
 export default hot(module)(Search);
