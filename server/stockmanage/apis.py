c3d2f640dcccce43806bb3ec71fef0be2198765f
Henu <hyun0404woo@gmail.com>
2021-11-07 23:34:40 +0900

    get code list..

--- server/stockmanage/apis.py
+++ server/stockmanage/apis.py
@@ -186,7 +186,7 @@ class DailyPriceApi(PublicApiMixin, APIView):
         
         data = {}
         data['code'] = company_code
-        return HttpResponse(company_code, status=status.HTTP_200_OK)
+        return HttpResponse(company_code[0], status=status.HTTP_200_OK)
         for code in company_code:
             company = Company.objects.filter(stock_code=code)
             
