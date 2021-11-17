5d19e7d316d46237274b5f180d24313b9b0813bf
Henu <hyun0404woo@gmail.com>
2021-11-18 00:32:27 +0900

    crwaling fs text

--- server/crawling/dart_crawling.py
+++ server/crawling/dart_crawling.py
@@ -124,6 +124,7 @@ def Get_Amount_Data(api_key,corp_code,year,quarter,link_state, link_model):
         bs_soup = BeautifulSoup(bs_res.text, "lxml") # html.parser 도 가능
 
         print(link_model.quarter.year.company.corp_name)
+        print(link_state, link_model.quarter, link_model.quarter.year)
         fs_unit = bs_soup.find("table").find_all('p')[-1]
         link_model.unit = fs_unit.text
         link_model.save()
