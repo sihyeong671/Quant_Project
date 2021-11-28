f00c65c88887d673126e416669411630b39c6345
Henu <hyun0404woo@gmail.com>
2021-11-28 14:15:10 +0900

    not allowed ne lookup in FloatField

--- server/stockmanage/utils.py
+++ server/stockmanage/utils.py
@@ -27,7 +27,7 @@ def getCaseData(case, condition, queryset):
     ## |  Case Field : ROE |
     ## =====================
     if case[0] == "ROE":
-        condition.add(Q(ROE__ne=0), Q.AND)
+        condition.add(~Q(ROE=0), Q.AND)
         if case[1] == 1:
             # 상위
             queryset = queryset.filter(
@@ -59,7 +59,7 @@ def getCaseData(case, condition, queryset):
     ## |  Case Field : ROA |
     ## =====================
     if case[0] == "ROA":
-        condition.add(Q(ROA__ne=0), Q.AND)
+        condition.add(~Q(ROA=0), Q.AND)
         if case[1] == 1:
             # 상위
             queryset = queryset.filter(
@@ -91,7 +91,7 @@ def getCaseData(case, condition, queryset):
     ## |  Case Field : GPA |
     ## =====================
     if case[0] == "GPA":
-        condition.add(Q(GPA__ne=0), Q.AND)
+        condition.add(~Q(GPA=0), Q.AND)
         if case[1] == 1:
             # 상위
             queryset = queryset.filter(
